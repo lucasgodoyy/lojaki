a55e47271c144a99a114c24b91ddbffbfd31711d
lucasgodoyy <ehlucasgodoy10@gmail.com>
2023-12-05 12:07:52 -0300

    Commit inicial

---
+++ src/main/java/lojaki/lojavirtual/LojakiApplication.java
@@ -0,0 +1,13 @@
+package lojaki.lojavirtual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojakiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojakiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lojaki/lojavirtual/LojakiApplicationTests.java
@@ -0,0 +1,13 @@
+package lojaki.lojavirtual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojakiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
