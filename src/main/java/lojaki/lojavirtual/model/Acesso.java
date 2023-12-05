dda0d9ad45af29200fcfe5ca1b60843772c34865
lucasgodoyy <ehlucasgodoy10@gmail.com>
2023-12-05 14:06:43 -0300

    Implementação de classes de modelo

--- src/main/java/lojaki/lojavirtual/model/Acesso.java
+++ src/main/java/lojaki/lojavirtual/model/Acesso.java
@@ -1,6 +1,5 @@
 package lojaki.lojavirtual.model;
 
-import java.io.Serializable;
 import java.util.Objects;
 
 import javax.persistence.Column;
@@ -11,23 +10,28 @@ import javax.persistence.Id;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
+import org.springframework.security.core.GrantedAuthority;
+
 @Entity
-@Table(name = "categoria_produto")
-@SequenceGenerator(name = "seq_categoria_produto", sequenceName = "seq_categoria_produto", allocationSize = 1, initialValue = 1)
-public class CategoriaProduto implements Serializable {
+@Table(name = "acesso")
+@SequenceGenerator(name = "seq_acesso", sequenceName = "seq_acesso", allocationSize = 1, initialValue = 1)
+public class Acesso implements GrantedAuthority {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_categoria_produto")
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_acesso")
 	private Long id;
-	
-	@Column(name = "nome_descricao", nullable = false)
-	private String nomeDesc;
 
 	
-	
-	
+	@Column(nullable = false)
+	private String descricao;
+
+	@Override
+	public String getAuthority() {
+		return this.descricao;
+	}
+
 	public Long getId() {
 		return id;
 	}
@@ -36,12 +40,12 @@ public class CategoriaProduto implements Serializable {
 		this.id = id;
 	}
 
-	public String getNomeDesc() {
-		return nomeDesc;
+	public String getDescricao() {
+		return descricao;
 	}
 
-	public void setNomeDesc(String nomeDesc) {
-		this.nomeDesc = nomeDesc;
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
 	}
 
 	@Override
@@ -57,12 +61,8 @@ public class CategoriaProduto implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		CategoriaProduto other = (CategoriaProduto) obj;
+		Acesso other = (Acesso) obj;
 		return Objects.equals(id, other.id);
 	}
-	
-	
-	
-	
 
 }
--- src/main/java/lojaki/lojavirtual/model/MarcaProduto.java
+++ src/main/java/lojaki/lojavirtual/model/MarcaProduto.java
@@ -3,6 +3,7 @@ package lojaki.lojavirtual.model;
 import java.io.Serializable;
 import java.util.Objects;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -21,6 +22,8 @@ public class MarcaProduto implements Serializable {
 	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_marca_produto")
 	private Long id;
 
+	
+	@Column(nullable = false)
 	private String nomeDesc;
 
 	
