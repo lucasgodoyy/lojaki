a77d448aa84879a64698b1ad6314b7361d4dd3dc
lucasgodoyy <ehlucasgodoy10@gmail.com>
2023-12-05 21:46:56 -0300

    Criando classes de modelo

--- src/main/java/lojaki/lojavirtual/LojakiApplication.java
+++ src/main/java/lojaki/lojavirtual/LojakiApplication.java
@@ -2,8 +2,10 @@ package lojaki.lojavirtual;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "lojaki.lojavirtual.model")
 public class LojakiApplication {
 
 	public static void main(String[] args) {
