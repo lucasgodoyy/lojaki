034f5949d6a0f7f63375da23563a80e07aa6d85e
lucasgodoyy <ehlucasgodoy10@gmail.com>
2023-12-07 19:15:57 -0300

    Finalização do token JWT

--- src/main/java/lojaki/lojavirtual/security/JWTTokenAutenticacaoService.java
+++ src/main/java/lojaki/lojavirtual/security/JWTTokenAutenticacaoService.java
@@ -1,15 +1,27 @@
 package lojaki.lojavirtual.security;
 
+import static java.util.Objects.isNull;
+import static java.util.Objects.nonNull;
+
 import java.io.IOException;
 import java.util.Date;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.SignatureException;
+import lojaki.lojavirtual.ApplicationContextLoad;
+import lojaki.lojavirtual.model.Usuario;
+import lojaki.lojavirtual.repository.UsuarioRepository;
 
 
 
@@ -40,13 +52,64 @@ public class JWTTokenAutenticacaoService {
 		
 		response.addHeader(HEADER_STRING, token);
 		
+		liberarCorsPolicy(response);
 		
 		response.getWriter().write("{\"Authorization\": \"" + token + "\"}");
 	}
 	
 	
+	public Authentication getAuthentication(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		String token = request.getHeader(HEADER_STRING);
+		
+		try {
+		
+			if (nonNull(token)) {
+				String tokenLimpo = token.replace(TOKEN_PREFIX, "").trim();
+				String user = Jwts.parser()
+					.setSigningKey(SECRET)
+					.parseClaimsJws(tokenLimpo) 
+					.getBody() /*Extraindo o usuario do token que fica no Body*/
+					.getSubject();
+				
+				if (nonNull(user)){
+					Usuario usuario = ApplicationContextLoad.getApplicationContext()
+						.getBean(UsuarioRepository.class)
+						.findUserByLogin(user);
+					
+					if (nonNull(usuario)) {
+						return new UsernamePasswordAuthenticationToken(
+							usuario.getLogin(), usuario.getSenha(), usuario.getAuthorities()
+						);
+					}
+				}
+			}
+		} catch(SignatureException e) {
+			response.getWriter().write("O token está inválido!");
+		} catch(ExpiredJwtException e) {
+			response.getWriter().write("O token está expirado! Efetue o login novamente!");
+		} catch(MalformedJwtException e) {
+			response.getWriter().write("O token está mal formado! Utilize um outro token!");
+		} finally {
+			liberarCorsPolicy(response);
+		}
+		return null;
+	}
 	
 	
+	private void liberarCorsPolicy(HttpServletResponse response) {
+		if (isNull(response.getHeader("Access-Control-Allow-Origin"))) {
+				response.addHeader("Access-Control-Allow-Origin", "*");		
+		}
+		if (isNull(response.getHeader("Access-Control-Allow-Headers"))) {
+				response.addHeader("Access-Control-Allow-Headers", "*");		
+		}
+		if (isNull(response.getHeader("Access-Control-Request-Headers"))) {
+				response.addHeader("Access-Control-Request-Headers", "*");		
+		}
+		if (isNull(response.getHeader("Access-Control-Allow-Methods"))) {
+				response.addHeader("Access-Control-Allow-Methods", "*");		
+		}
+	}
 	
 
 }
--- src/main/java/lojaki/lojavirtual/security/WebConfigSecurity.java
+++ src/main/java/lojaki/lojavirtual/security/WebConfigSecurity.java
@@ -7,10 +7,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import lojaki.lojavirtual.service.ImplementacaoUserDetailsService;
 
@@ -25,19 +29,34 @@ public class WebConfigSecurity extends WebSecurityConfigurerAdapter implements H
 	
 	
 	
+	
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf()
+			.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+			.disable().authorizeRequests().antMatchers("/").permitAll()
+			.antMatchers("/index").permitAll()
+			.antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+			.anyRequest().authenticated().and().logout().logoutSuccessUrl("/index")
+			.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+			.and()
+			.addFilterAfter(
+					new JWTLoginFilter("/login", authenticationManager()), UsernamePasswordAuthenticationFilter.class
+							)
+			.addFilterBefore(
+					new JwtApiAutenticacaoFilter(), UsernamePasswordAuthenticationFilter.class
+							);
+	}
+	
+	
+
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(implementacaoUserDetailsService)
 			.passwordEncoder(new BCryptPasswordEncoder());
 		
 	}
-	
-	
-	
-	
-	
-	
-	
 	
 	
 	
