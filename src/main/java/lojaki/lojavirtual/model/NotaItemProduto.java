7c543b483aaa9d9c9a0bc018fc0d8fa93ac7b97f
lucasgodoyy <ehlucasgodoy10@gmail.com>
2023-12-13 14:46:36 -0300

    End-Point NotaItemProduto

--- src/main/java/lojaki/lojavirtual/model/NotaItemProduto.java
+++ src/main/java/lojaki/lojavirtual/model/NotaItemProduto.java
@@ -40,9 +40,9 @@ public class NotaItemProduto implements Serializable{
 	private NotaFiscalCompra notaFiscalCompra;
 	
 	
-	@ManyToOne(targetEntity = Pessoa.class)
+	@ManyToOne(targetEntity = PessoaJuridica.class)
 	@JoinColumn(name = "empresa_id", nullable = false, foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "empresa_id_fk"))
-	private Pessoa empresa;
+	private PessoaJuridica empresa;
 	
 
 	public Long getId() {
@@ -80,11 +80,11 @@ public class NotaItemProduto implements Serializable{
 	
 	
 	
-	public Pessoa getEmpresa() {
+	public PessoaJuridica getEmpresa() {
 		return empresa;
 	}
 
-	public void setEmpresa(Pessoa empresa) {
+	public void setEmpresa(PessoaJuridica empresa) {
 		this.empresa = empresa;
 	}
 
